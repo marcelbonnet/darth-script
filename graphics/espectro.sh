ffe776239e876e51caeeb89776ea2858cd92e562
Marcel Bonnet <marcelbonnet@gmail.com>
2018-04-03 16:06:12 -0300

    Permite resize do espectro. Usa cores HEX

--- graphics/espectro.sh
+++ graphics/espectro.sh
@@ -3,7 +3,7 @@
 #convert espectro.png -fill red -draw "line 100,280 100,339 line 200,280, 200,339" -annotate +0+100 "200" -annotate +50+120 "220"  to.png
 
 function usage(){
-	printf "%s [freq ini] [bw MHz] [imagem] [saída: nome.extensao]\n" `basename $0`
+	printf "%s [freq ini] [bw MHz] [imagem] [saída: nome.extensao] [resize]\n" `basename $0`
 	exit 1
 }
 
@@ -13,19 +13,20 @@ freqi=$1
 bw=$2
 img=$3
 saida=$4
-w=$(file $img | cut -d ' ' -f5 )
-h=$(file $img | cut -d ' ' -f7 | grep -Eo "[0-9]*")
+resize=$5
+w=$(file $img | cut -d ' ' -f5 ) 					; w=$(echo "$w * $resize" | bc)
+h=$(file $img | cut -d ' ' -f7 | grep -Eo "[0-9]*")	; h=$(echo "$h * $resize" | bc)
 hi=$(printf "%i" `echo "scale=0; $h * 0.7" | bc` 2>/dev/null)
 pxfreq=$(echo "scale=3; $bw/$w " | bc)
-salto=5	#pixel
+salto=10	#pixel
 
-opts=""
-yi=0
-cores=("white" "red" "green" "blue" "orange" "lightblue" "pink")
+opts="-resize $(echo "$resize * 100" | bc)% "
+yi=10
+cores=("white" "red" "#00ff00ff" "#3366ffff" "orange" "#ff0099ff" "#9900ffff" )
 corindex=0
 for((x=0;x<=$w;x=x+$salto)); do
 	# altura do label
-	[[ $yi -gt $hi ]] && yi=0
+	[[ $yi -gt $hi ]] && yi=10
 	yi=$(echo "scale=0; $yi + $h/8 + 10" | bc )
 	# label
 	fi=$(echo "scale=3; $freqi + $pxfreq * $x  " | bc)
@@ -34,7 +35,7 @@ for((x=0;x<=$w;x=x+$salto)); do
 	[[ $corindex -gt  ${#cores} ]] && corindex=0
 	cor=${cores[$corindex]}
 	# desenho e labels:
-	opts="$opts -fill $cor -draw \"line $x,$hi $x,$h \" -annotate +${x}+${yi} \"$fi\" "
+	opts="$opts -fill \"$cor\" -draw \"line $x,$hi $x,$h \" -annotate +${x}+${yi} \"$fi\" "
 done
 
 eval convert $img $opts $saida
